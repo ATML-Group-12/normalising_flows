ec1c0440183e88b56bee80200f9cd3dbc533192e
johnryan465 <johnryan465@gmail.com>
2022-03-30 16:44:15 +0000

    Fixed nans during training

--- flows/loss/elbo.py
+++ flows/loss/elbo.py
@@ -3,7 +3,10 @@ import torch
 from torch.nn import Module
 
 
-def FlowELBO(true_density: Callable[[torch.Tensor], torch.Tensor], var_dist: torch.distributions.TransformedDistribution, num_samples: int, epoch: int) -> torch.Tensor:
+def FlowELBO(
+        true_density: Callable[[torch.Tensor],
+                               torch.Tensor],
+        var_dist: torch.distributions.TransformedDistribution, num_samples: int, epoch: int) -> torch.Tensor:
     """
     F(x) = E_q0(z0)[ln qK(zK) - log p(x, zK)]
             = E_q0(z0)[ln q0(z0)] 
@@ -11,6 +14,8 @@ def FlowELBO(true_density: Callable[[torch.Tensor], torch.Tensor], var_dist: tor
             - Eq0(z0)[log p(x, zK)]
     """
     zk = var_dist.rsample((num_samples,))
+    with torch.no_grad():
+        true_sample = torch.log(torch.clamp(true_density(zk.T), min=1e-12, max=None))
     # print(zk.shape)
-    w = var_dist.log_prob(zk) - (min(1, 0.01 + epoch/10000) * torch.log(true_density(zk.T)))
-    return w.mean() 
+    w = var_dist.log_prob(zk) - (min(1, 0.01 + epoch/10000) * true_sample)
+    return w.mean()
