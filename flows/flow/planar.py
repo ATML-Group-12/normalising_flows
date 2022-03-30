0a4c535507d7947aa91bfcc9e8ed93443b9f93a5
johnryan465 <johnryan465@gmail.com>
2022-03-30 20:21:37 +0000

    Added flows

--- flows/flow/planar.py
+++ flows/flow/planar.py
@@ -1,48 +1,5 @@
-from typing import overload
-import torch
-from torch.distributions import constraints
-from torch.distributions.transforms import Transform
+from pyro.distributions.transforms.planar import Planar
 
 
-class PlanarFlow(Transform):
-    """
-    This class is an implementaion of the idea of the Planar flow from 4.1 (https://arxiv.org/pdf/1505.05770.pdf)
-
-    f(z) = z + u h(w^T z + b)
-    - b \in R
-    - w \in R^D
-    - u \in R^D
-    - h is elementwise no linearity (tanh is used)
-    """
-
-    def __init__(self, size: int) -> None:
-        self.b = torch.nn.Parameter(torch.zeros(1))
-        self.w = torch.nn.Parameter(torch.zeros((size, 1)))
-        self.u = torch.nn.Parameter(torch.zeros((size, 1)))
-        super().__init__()
-
-    @staticmethod
-    def h(x: torch.Tensor) -> torch.Tensor:
-        return torch.tanh(x)
-    
-    @staticmethod
-    def hprime(x: torch.Tensor) -> torch.Tensor:
-        return 1 - torch.square(torch.tanh(x))
-
-    def _call(self, x: torch.Tensor) -> torch.Tensor:
-        r = self.h(torch.transpose(self.w, -2, -1) @ x + self.b)
-        return x + self.u @ r
-
-    @constraints.dependent_property(is_discrete=False)
-    def domain(self) -> constraints.Constraint:
-        return constraints.real_vector
-
-    @constraints.dependent_property(is_discrete=False)
-    def codomain(self) -> constraints.Constraint:
-        return constraints.real_vector
-    
-    def psi(self, x: torch.Tensor) -> torch.Tensor:
-        return self.hprime( (self.w.T @ x) + self.b)
-
-    def log_abs_det_jacobian(self, x: torch.Tensor) -> torch.Tensor:
-        return torch.log(torch.abs(1 + self.u.T @ self.psi(x)))
+class PlanarFlow(Planar):
+    pass
--- flows/flow/radial.py
+++ flows/flow/radial.py
@@ -1,9 +1,10 @@
 import torch
 from torch.distributions import constraints
 from torch.distributions.transforms import Transform
+from pyro.distributions.torch_transform import TransformModule
 
 
-class RadialFlow(Transform):
+class RadialFlow(TransformModule):
     """
     This class is an implementaion of the idea of the Radial flow from 4.1 (https://arxiv.org/pdf/1505.05770.pdf)
 
@@ -14,25 +15,59 @@ class RadialFlow(Transform):
     - r = |z - z_0|
     - h = 1/(a + r)
     """
+    domain = constraints.real_vector
+    codomain = constraints.real_vector
+    bijective = True
 
     def __init__(self, size: int) -> None:
         super().__init__()
-        self.b = torch.nn.Parameter(torch.zeros(1))
-        self.a = torch.nn.Parameter(torch.ones(1))
-        self.z_0 = torch.nn.Parameter(torch.zeros(size))
+        self.d = size
+        self.beta = torch.nn.Parameter(torch.zeros(1))
+        self.alpha = torch.nn.Parameter(torch.ones(1))
+        self.z_0 = torch.nn.Parameter(torch.zeros(self.d))
 
-    @constraints.dependent_property(is_discrete=False)
-    def domain(self) -> constraints.Constraint:
-        return constraints.real_vector
+    def _ha(self, r: torch.Tensor) -> torch.Tensor:
+        return 1 / (self.alpha + r)
 
-    @constraints.dependent_property(is_discrete=False)
-    def codomain(self) -> constraints.Constraint:
-        return constraints.real_vector
+    def _hprimea(self, u: torch.Tensor, r: torch.Tensor) -> torch.Tensor:
+        return -u / (torch.square(self.alpha + r) * r)
 
     def _call(self, x: torch.Tensor) -> torch.Tensor:
         g = x - self.z_0
-        r = torch.abs(g)
-        return x + (- self.a + torch.log(1 + torch.exp(self.b))) * (1 / (self.a + r)) * g
+        r = torch.norm(g, dim=-1, keepdim=True)
+        return x + (self.beta) * (1 / (self.alpha + r)) * g
 
-    def log_abs_det_jacobian(self, x: torch.Tensor) -> torch.Tensor:
-        return x
+    def log_abs_det_jacobian(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        beta = self.beta
+        g = x - self.z_0
+        r = torch.abs(g)
+
+        h = self._ha(r)
+        h_prime = self._hprimea(g, r)
+
+        return (self.d - 1) * torch.log(1 + self.beta * h) + torch.log(1 + (self.beta * h) + (self.beta * h_prime * r))
+
+    def _inverse(self, y: torch.Tensor) -> torch.Tensor:
+        """
+        From (26)
+
+        | y - z_0 | = r ( 1 + b / (a + r))
+                t = r ( 1 + b / (a + r))
+                t = r ( a + r + b) / (a + r)
+        (a + r) t = r ( a + r + b)
+          at + tr = ar + r^2 + br
+                0 = r^2 + (a + b - t)r + (-at)
+                r = (-(a + b - t) +- sqrt((a + b - t)^2 - 4(-at))) / 2
+
+        if (-at) < 0 we will have only one positive solution:
+            but both a and t will be positive a by defintion t as it is a norm
+        
+        we take the positive solution
+                r = ((t - a - b) + sqrt((a + b - t)^2 + 4at)) / 2
+
+                z = (a + r)/(a + r + b) y + b/(a + r + b) z_0
+        """
+        t = torch.norm(y - self.z_0, dim=-1)
+        b = self.beta + self.alpha - t
+        r = ( (-b) + torch.sqrt( torch.square(b) + 4 * self.alpha * t)) / 2
+        return ((self.alpha + r) / (self.alpha + self.beta + r )) * y + (self.beta / (self.alpha + self.beta + r )) * self.z_0
