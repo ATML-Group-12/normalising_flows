24920a38a435c5f8cc3f86dc3f7820eacfd281b9
johnryan465 <johnryan465@gmail.com>
2022-03-31 14:18:13 +0100

    Added GP

--- flows/embedding/gp.py
+++ flows/embedding/gp.py
@@ -1,5 +1,32 @@
+import torch
 from embedding.embedding import Embedding
+import gpytorch
 
 
-class GP(Embedding):
-    pass
+class VariationalGP(Embedding, gpytorch.models.ApproximateGP):
+    def __init__(self, inducing_points: torch.Tensor, num_latent: int, num_tasks: int):
+        variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(
+            inducing_points.size(-2), batch_shape=torch.Size([num_latent])
+        )
+
+        variational_strategy = gpytorch.variational.LMCVariationalStrategy(
+            gpytorch.variational.VariationalStrategy(
+                self, inducing_points, variational_distribution, learn_inducing_locations=True
+            ),
+            num_tasks=num_tasks,
+            num_latents=num_latent,
+            latent_dim=-1
+        )
+
+        super().__init__(variational_strategy)
+
+        self.mean_module = gpytorch.means.ConstantMean(batch_shape=torch.Size([num_latent]))
+        self.covar_module = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel(batch_shape=torch.Size([num_latent])),
+            batch_shape=torch.Size([num_latent])
+        )
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
