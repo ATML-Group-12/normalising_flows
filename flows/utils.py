e69d5bab6084e31ca3be17ebcb1dab864f05d118
LyndonFan <lyndon0808@gmail.com>
2022-04-01 22:25:20 +0100

    fix shape error

--- flows/utils.py
+++ flows/utils.py
@@ -1,16 +1,26 @@
 import torch
+import torch.nn as nn
 
-
-class MaxoutLayer:
+class MaxoutLayer(nn.Module):
     def __init__(self, d_in, d_out, pool_size):
+        super(MaxoutLayer, self).__init__()
         self.d_in, self.d_out, self.pool_size = d_in, d_out, pool_size
         self.lin = torch.nn.Linear(d_in, d_out * pool_size)
 
     def forward(self, x):
-        shape = list(x.size())
+        shape = list(x.shape)
         shape[-1] = self.d_out
-        max_dim = -1
+        shape.append(self.pool_size)
         x = self.lin(x)
         x = x.view(*shape)
-        x = x.max(max_dim)[0]
+        x = x.max(-1)[0]
         return x
+
+# if __name__ == '__main__':
+#     m = MaxoutLayer(8, 6, 4)
+#     x = torch.randn(8)
+#     y = m(x)
+#     assert y.size() == (6,)
+#     x = torch.randn(2,8)
+#     y = m(x)
+#     assert y.size() == (2,6)
