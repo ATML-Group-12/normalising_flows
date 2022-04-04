9de2ad022324a656c146adbe08d8716f46260692
LyndonFan <lyndon0808@gmail.com>
2022-04-04 16:37:48 +0100

    log 5 imgs per run

--- experiments/one.py
+++ experiments/one.py
@@ -37,7 +37,7 @@ class Params:
     num_updates: int = 500000
     lr: float = 1e-5
     momentum: float = 0.9
-    save_images: bool = True
+    num_progress_images: int = 5
 
 
 
@@ -71,6 +71,9 @@ def run(params: Params):
     # pathlib.Path(f"runs/{params.name}/images").mkdir(parents=True, exist_ok=True) 
 
     NUM_STEPS = int(np.ceil(params.num_updates / NUM_PARAMETERS))
+    RECORD_EVERY = int(np.floor(NUM_STEPS / (params.num_progress_images-1))) if params.num_progress_images > 1 else NUM_STEPS+1
+    if params.num_progress_images in [1,2]:
+        RECORD_EVERY = NUM_STEPS - 1
     pbar = tqdm(range(NUM_STEPS))
     for step in pbar:
         optimizer.zero_grad()
@@ -82,7 +85,7 @@ def run(params: Params):
         writer.add_scalar("embedding_covariance_determinant", torch.det(model.embedding.cov).item(), step * NUM_PARAMETERS)
         writer.add_scalar("embedding_mean_magnitude", torch.norm(torch.abs(model.embedding.mean)).item(), step * NUM_PARAMETERS)
 
-        if params.save_images and step % 2000 == 0:
+        if params.num_progress_images>0 and step % RECORD_EVERY == 0:
             samples = model(torch.ones([3000,1])).sample((1,)).detach()
             samples = samples.view((3000,dims))
             sns.kdeplot(x=samples[:,0].detach().numpy(), y=samples[:,1].detach().numpy(), cmap="Blues", shade=True)
@@ -95,8 +98,9 @@ if __name__ == "__main__":
     energy_functions = {"u1": u1, "u2": u2, "u3": u3, "u4": u4}
     layer_types = {"radialflow": RadialFlow, "planarflow": PlanarFlow, "niceorthogonal": NiceOrthogonal, "nicepermutation": NicePermutation}
     flow_lengths = [2, 8, 32]
-    for layer_name, layer_type in layer_types.items():
-        for function_name, function in energy_functions.items():
+    for function_name, function in energy_functions.items():
+        for layer_name, layer_type in layer_types.items():
+            if "flow" in layer_name: continue
             for flow_length in flow_lengths:
                 params = Params(
                     layer_type=layer_type,
